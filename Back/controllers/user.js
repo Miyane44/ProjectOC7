37a1744418a887918574b632a255f1ee5257518f
Miyane44 <elodie-mouquet@orange.fr>
2023-11-03 08:38:19 +0100

    add validators to signup form

--- Back/controllers/user.js
+++ Back/controllers/user.js
@@ -7,17 +7,36 @@ const jwtSecretKey = process.env.jwtSecretKey;
 const User = require('../models/User');
 
 exports.signup = (req, res, next) => {
-    bcrypt.hash(req.body.password, 10)
-    .then(hash => {
-        const user = new User({
-            email : req.body.email,
-            password: hash
-        });
-        user.save()
-        .then(() => res.status(201).json({ message: 'Utilisateur créé.' }))
-        .catch(error => res.status(400).json({ error }))
+    User.findOne({email: req.body.email})
+    .then(existingUser => {
+        if (existingUser) {
+            return res.status(400).json({ message: 'Cet email est déjà enregistré.' });
+        }
+        if (req.body.password.length < 8) {
+            return res.status(400).json({ message: 'Le mot de passe doit contenir au moins 8 caractères.' });
+        }
+        bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+            const user = new User({
+                email : req.body.email,
+                password: hash
+            });
+            
+            const regex = /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
+            const isEmailValid = regex.test(user.email);
+            
+            if (isEmailValid) {
+                user.save()
+                .then(() => res.status(201).json({ message: 'Utilisateur créé.' }))
+                .catch(error => res.status(400).json({ error }));
+            } else {
+                return res.status(400).json({ message: 'Merci de renseigner un email valide.' });
+            }
+        })
+        .catch(error => res.status(500).json({ error }));
     })
-    .catch(error => res.status(500).json({ error }));
+    .catch(error => res.status(500).json({ error }))
+    
 }
 
 exports.login = (req, res, next) => {
